ce9d16430fc260f94792bcb3fa43baba57c9b5e5
wqy_home <wqy.mail@foxmail.com>
2020-02-22 18:21:57 +0800

    init

--- src/components/demo/children.jsx
+++ src/components/demo/children.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react"
+import { Button } from 'antd-mobile';
 class Children extends Component {
   render() {
     return (
+      <div>
+        <Button>123</Button>
       <div>this.children.jsx</div>
+      </div>
     )
   }
 }
